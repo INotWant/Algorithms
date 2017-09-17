1b081dd9b3c3a1466085cf86cf41142d696cd7c9
jie8888 <kiss.xiaojie15@gmail.com>
2017-09-17 21:34:23 +0800

    最大子数组（一）

---
+++ src/chapter3/ZDZSZ.java
@@ -0,0 +1,71 @@
+package chapter3;
+
+/**
+ * @author kissx on 2017/9/17.
+ *         最大子数组问题
+ */
+public class ZDZSZ {
+
+    private static class Node {
+        int start;
+        int end;
+        int value;
+
+        Node(int start, int end, int value) {
+            this.start = start;
+            this.end = end;
+            this.value = value;
+        }
+    }
+
+    private static Node zdzsz(int[] array, int start, int end) {
+        if (start == end)
+            return new Node(start, end, array[start]);
+        else {
+            int mid = (start + end) / 2;
+            Node n1 = zdzsz(array, start, mid);
+            Node n2 = zdzsz(array, mid + 1, end);
+            Node n3 = center(array, start, mid, end);
+            if (n1.value >= n2.value && n1.value >= n3.value)
+                return n1;
+            else if (n2.value > n1.value && n2.value > n3.value)
+                return n2;
+            else
+                return n3;
+        }
+    }
+
+    private static Node center(int[] array, int start, int mid, int end) {
+        int currentMax = array[mid];
+        int sum = currentMax;
+        int s = mid;
+        for (int i = mid - 1; i >= start; i--) {
+            sum += array[i];
+            if (sum > currentMax) {
+                s = i;
+                currentMax = sum;
+            }
+        }
+        sum = currentMax;
+        int e = mid;
+        for (int i = mid + 1; i <= end; i++) {
+            sum += array[i];
+            if (sum > currentMax) {
+                e = i;
+                currentMax = sum;
+            }
+        }
+        return new Node(s, e, currentMax);
+    }
+
+    // --------------------------------分割线--------------------------------
+
+
+
+    public static void main(String[] args) {
+//        int[] array = {13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7};
+        int[] array = {-2, -1, -5};
+        Node n = zdzsz(array, 0, array.length - 1);
+        System.out.println("start::" + n.start + " end::" + n.end + " value::" + n.value);
+    }
+}
