f77511486a4e784ee47b47bfc6aec2679a3e7aeb
jie8888 <kiss.xiaojie15@gmail.com>
2017-10-17 16:45:40 +0800

    [红黑树(END)]

--- src/chapter13/RedBlackTree.java
+++ src/chapter13/RedBlackTree.java
@@ -9,14 +9,20 @@ import org.junit.Test;
  * 3，叶结点颜色为黑色；
  * 4，红色结点的子节点颜色为黑色；
  * 5，从某结点到叶节点的黑色结点数相同；
- * (细节决定成败)
+ *
+ * 总结：红黑树的插入、删除操作比较复杂，情况较多。但是，若认真分析、细心地操作每个“引用”（指针），说难也不难。
+ *      比如，红黑树的插入操作实现中，出现了两个错误：
+ *      【1】在插入出现一种情况中，我把父与子弄乱了。在写的时候有点注意到但是没有再仔细检查。
+ *      【2】旋转后一些引用内容发现了变化导致出错。其实，在之前也注意到了，还是没有认真对待。
+ *      “细节决定成败！”
+ *
  *
  * @author kissx on 2017/10/16.
  */
 public class RedBlackTree<T extends Comparable<T>> {
 
-    private Node<T> root = null;
-    private final Node<T> nullNode = new Node<>();
+    private Node<T> root = null;    // 根节点
+    private final Node<T> nullNode = new Node<>(); // NIL节点（哨兵 T.nail ）
 
     public static class Node<T> {
         Node<T> pNode;
@@ -46,6 +52,11 @@ public class RedBlackTree<T extends Comparable<T>> {
         }
     }
 
+    /**
+     * 插入：前面的操作与二叉搜索树插入类似，这样插入后可能破话了红黑树的性质，后面的操作是修改颜色和指针结构以使得恢复
+     *
+     * @param value value 值
+     */
     public void insert(T value) {
         Node<T> pNode = nullNode;
         Node<T> cNode = root;
@@ -74,16 +85,17 @@ public class RedBlackTree<T extends Comparable<T>> {
         insertFixUp(node);
     }
 
+    // 插入修改
     private void insertFixUp(Node<T> z) {
         while (z.pNode.isRed) {
             if (z.pNode.pNode.lcNode == z.pNode) {
                 Node<T> y = z.pNode.pNode.rcNode;
-                if (y.isRed) {                       // case 1
+                if (y.isRed) {                                              // case 1
                     y.isRed = false;
                     z.pNode.isRed = false;
                     z.pNode.pNode.isRed = true;
                     z = z.pNode.pNode;
-                } else if (z.pNode.rcNode == z) {                            // case 2
+                } else if (z.pNode.rcNode == z) {                           // case 2
                     z.pNode.rcNode = z.lcNode;
                     z.pNode.rcNode.pNode = z.pNode;
                     y = z.pNode;
@@ -110,7 +122,7 @@ public class RedBlackTree<T extends Comparable<T>> {
                     z.pNode.isRed = false;
                     z.pNode.pNode.isRed = true;
                     z = z.pNode.pNode;
-                } else if (z.pNode.lcNode == z) {                            // case 2
+                } else if (z.pNode.lcNode == z) {                           // case 2
                     z.pNode.lcNode = z.rcNode;
                     z.pNode.lcNode.pNode = z.pNode;
                     y = z.pNode;
@@ -135,6 +147,7 @@ public class RedBlackTree<T extends Comparable<T>> {
         this.root.isRed = false;
     }
 
+    // 左旋（逆时针，以 n2 为轴）
     private void leftRotate(Node n1, Node n2) {
         transplant(n1, n2);
         n1.rcNode = n2.lcNode;
@@ -143,6 +156,7 @@ public class RedBlackTree<T extends Comparable<T>> {
         n2.lcNode.pNode = n2;
     }
 
+    // 右旋（顺时针，以 n2 为轴）
     private void rightRotate(Node n1, Node n2) {
         transplant(n1, n2);
         n1.lcNode = n2.rcNode;
@@ -151,6 +165,7 @@ public class RedBlackTree<T extends Comparable<T>> {
         n2.rcNode.pNode = n2;
     }
 
+    // 用 v 子树 代替 u 子树（只更改最上层指针结构）
     @SuppressWarnings("unchecked")
     private void transplant(Node u, Node v) {
         if (this.root == u) {
@@ -163,8 +178,13 @@ public class RedBlackTree<T extends Comparable<T>> {
         v.pNode = u.pNode;
     }
 
-    /*
 
+    /**
+     * 删除节点
+     *
+     * @param z 待删除的结点
+     * @return 删除成功返回已删除的结点，否则返回 NULL
+     */
     public Node<T> delete(Node<T> z) {
         if (z != nullNode && z != null) {
             Node<T> x = nullNode;
@@ -196,11 +216,12 @@ public class RedBlackTree<T extends Comparable<T>> {
                 Node<T> temp = y.pNode;
                 transplant(z, y);
                 y.lcNode = z.lcNode;
-                y.lcNode.pNode = z;
+                y.lcNode.pNode = y;
                 if (temp != z) {
                     y.rcNode = z.rcNode;
                     y.rcNode.pNode = y;
                 }
+                y.isRed = z.isRed;
             }
             if (!isORed)
                 deleteFixUp(x);
@@ -208,38 +229,39 @@ public class RedBlackTree<T extends Comparable<T>> {
         return z;
     }
 
+    // 删除修改
     private void deleteFixUp(Node<T> x) {
-        while (x != root && !x.isRed) {
+        while (x != root && !x.isRed()) {
             if (x == x.pNode.lcNode) {
                 Node<T> w = x.pNode.rcNode;
-                if (w.isRed) {
+                if (w.isRed()) {                                    // case 1
                     w.isRed = false;
                     x.pNode.isRed = true;
-                    leftRotate(x.pNode, x);
+                    leftRotate(w.pNode, w);
                     w = x.pNode.rcNode;
                 }
-                if (!w.lcNode.isRed && !w.rcNode.isRed) {
+                if (!w.lcNode.isRed() && !w.rcNode.isRed()) {       // case 2
                     w.isRed = true;
                     x = x.pNode;
                 } else {
-                    if (!w.rcNode.isRed) {
+                    if (!w.rcNode.isRed()) {                        // case 3
                         w.lcNode.isRed = false;
                         w.isRed = true;
-                        rightRotate(w, w.pNode);
+                        rightRotate(w, w.lcNode);
                         w = x.pNode.rcNode;
-                    }
+                    }                                               // case 4
                     w.isRed = x.pNode.isRed;
                     x.pNode.isRed = false;
                     w.rcNode.isRed = false;
-                    leftRotate(x.pNode, x);
+                    leftRotate(w.pNode, w);
                     x = this.root;
                 }
             } else {
                 Node<T> w = x.pNode.lcNode;
-                if (w.isRed) {
+                if (w.isRed()) {
                     w.isRed = false;
                     x.pNode.isRed = true;
-                    rightRotate(x.pNode, x);
+                    rightRotate(w.pNode, w);
                     w = x.pNode.lcNode;
                 }
                 if (!w.rcNode.isRed && !w.lcNode.isRed) {
@@ -249,19 +271,23 @@ public class RedBlackTree<T extends Comparable<T>> {
                     if (!w.lcNode.isRed) {
                         w.rcNode.isRed = false;
                         w.isRed = true;
-                        leftRotate(w, w.pNode);
+                        leftRotate(w, w.lcNode);
                         w = x.pNode.lcNode;
                     }
                     w.isRed = x.pNode.isRed;
                     x.pNode.isRed = false;
                     w.lcNode.isRed = false;
-                    rightRotate(x.pNode, x);
+                    rightRotate(w.pNode, w);
                     x = this.root;
                 }
             }
         }
+        x.isRed = false;
     }
 
+    /**
+     * @return n 子树的最小结点
+     */
     private Node<T> getMin(Node<T> n) {
         Node<T> cNode = n;
         Node<T> pNode = nullNode;
@@ -271,8 +297,6 @@ public class RedBlackTree<T extends Comparable<T>> {
         }
         return pNode;
     }
-
-    */
 
     /**
      * @param value value值
@@ -314,7 +338,7 @@ public class RedBlackTree<T extends Comparable<T>> {
         redBlackTree.insert(4);
         System.out.println(redBlackTree.toString());
     }
-/*
+
     @Test
     public void testDelete() {
         RedBlackTree<Integer> redBlackTree = new RedBlackTree<>();
@@ -327,10 +351,9 @@ public class RedBlackTree<T extends Comparable<T>> {
         redBlackTree.insert(2);
         redBlackTree.insert(11);
         redBlackTree.insert(8);
-        Node<Integer> n = redBlackTree.search(14);
+        Node<Integer> n = redBlackTree.search(5);
         redBlackTree.delete(n);
         System.out.println("-------------END-------------");
     }
-*/
 
 }
