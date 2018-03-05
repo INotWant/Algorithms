e5b481e1aa269b336e530f51cd22351e7761209e
inotwant <kiss.xiaojie15@gmail.com>
2018-03-05 20:24:39 +0800

    [P37]

---
+++ src/other/P37.java
@@ -0,0 +1,50 @@
+package other;
+
+/**
+ * url :: https://leetcode.com/problems/sudoku-solver/description/
+ * 暴力求解 （相同的思想、类似的结构。然而它更简洁，暴力的元素选择正确！）
+ *
+ * @author kissx on 2018/3/5.
+ */
+public class P37 {
+
+    public void solveSudoku(char[][] board) {
+        if (board == null || board.length == 0)
+            return;
+        solve(board);
+    }
+
+    public boolean solve(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {//trial. Try 1 through 9
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c; //Put c for this cell
+
+                            if (solve(board))
+                                return true; //If it's the solution return true
+                            else
+                                // 回溯
+                                board[i][j] = '.'; //Otherwise go back
+                        }
+                    }
+
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] != '.' && board[i][col] == c) return false; //check row
+            if (board[row][i] != '.' && board[row][i] == c) return false; //check column
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] != '.' &&
+                    board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false; //check 3*3 block
+        }
+        return true;
+    }
+
+}
